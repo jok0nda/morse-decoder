057f8fb08cc9e89d5f33b6ad2b955d378a9f6b3e
jok0nda <masha_rulezz@mail.ru>
2022-02-22 14:09:19 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,47 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+    let arr = expr.toString().split('');
+        for (i = 0; i < arr.length; i++) {
+             arr[i] += arr[i + 1]
+             arr.splice(i + 1, 1)
+        }
+    let slicedArr = [];
+    let result = [];
+    let word = [];
+        for (i = 0; i < arr.length; i += 5) {
+            slicedArr.push(arr.slice(i, i + 5)) 
+        }
+        for (i = 0; i < slicedArr.length; i++) {
+        for (n = 0; n < 5; n++) {
+            if (slicedArr[i][n] === '00') {
+                slicedArr[i].splice(n, 1)
+                n--
+            }
+            if (slicedArr[i][n] === '10') {
+             slicedArr[i][n] = '.'
+            }
+            if (slicedArr[i][n] === '11') {
+                slicedArr[i][n] = '-'
+            }
+            if (slicedArr[i][n] === '**') {
+                slicedArr[i][n] = 's'
+            }
+        }
+    result.push(slicedArr[i].join(''))
+    }
+    result.forEach((element) => {
+      if (element === 'sssss') {
+        element = ' '
+      } else {
+        element = MORSE_TABLE[element]
+      }
+        word.push(element)
+      })
+    
+    return word.join('')
 }
 
 module.exports = {
